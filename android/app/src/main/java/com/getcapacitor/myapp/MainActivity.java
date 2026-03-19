17926b5c68d67fd73655b6bd3edc0093f1a1536f
Tygo <tygo.gerritzen@student.vonknh.nl>
2026-03-19 10:05:10 +0100

    fix: hide status bar using WindowInsetsController for modern Android

--- android/app/src/main/java/com/getcapacitor/myapp/MainActivity.java
+++ android/app/src/main/java/com/getcapacitor/myapp/MainActivity.java
@@ -1,13 +1,25 @@
 package com.getcapacitor.myapp;
 
 import android.os.Bundle;
+import android.view.View;
 import androidx.activity.OnBackPressedCallback;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Hide status bar on modern Android (windowFullscreen XML flag is deprecated on API 30+)
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+        WindowInsetsControllerCompat insetsController =
+                new WindowInsetsControllerCompat(getWindow(), getWindow().getDecorView());
+        insetsController.hide(WindowInsetsCompat.Type.statusBars());
+        insetsController.setSystemBarsBehavior(
+                WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
 
         // Register via OnBackPressedDispatcher so it works on Android 13+ predictive
         // back gesture as well as older devices. The override of onBackPressed() alone
